d8984a4d384006a6d1fcc2fc2fe91ccad6d90ef6
maoe <maoe.maoe@gmail.com>
2010-02-24 00:02:31 +0900

    add snmp4clj.examples

--- src/snmp4clj.clj
+++ src/snmp4clj.clj
@@ -17,11 +17,3 @@
 
 (def simple-snmp-walk
   (partial simple-send snmp-walk))
-
-(defn simple-test []
-  (println (simple-snmp-get-next
-             "public" "udp:localhost/161" "1.3.6.1.2.1.31.1.1.1"))
-  (println (apply + (map #(count (.getVariableBindings %))
-                         (simple-snmp-walk
-                           "public" "udp:localhost/161"
-                           "1.3.6.1.2.1.31.1.1.1")))))
