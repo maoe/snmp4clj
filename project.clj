aeebfc0b950dcdc38de392c2b87542b3cc69c129
maoe <maoe.maoe@gmail.com>
2010-02-23 18:03:45 +0900

    use clojure-1.1.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject snmp4clj "0.0.1-SNAPSHOT"
   :description "SNMP API for Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [org.snmp4j/snmp4j "1.8.1"]]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]
                      [leiningen/lein-swank "1.1.0"]]
