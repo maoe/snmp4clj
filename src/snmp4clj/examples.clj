9666090e9509adf9a7259ed98898dac78c8ebaf8
maoe <maoe.maoe@gmail.com>
2010-03-09 14:00:54 +0900

    add basic async support and examples

--- src/snmp4clj/examples.clj
+++ src/snmp4clj/examples.clj
@@ -3,25 +3,32 @@
         snmp4clj.session
         snmp4clj.util)
   (:use clojure.contrib.pprint)
+  (:import [org.snmp4j.event ResponseListener])
   (:gen-class))
 
 (defn -main []
   (with-snmp-session session
-    ;; retrieve a name of the first network interface
-    (->> (snmp-get-next session
-           :address "udp:localhost/161"
-           "1.3.6.1.2.1.31.1.1.1")
+    (println "retrieve a name of the first network interface")
+    (->> (snmp-get-next session "1.3.6.1.2.1.31.1.1.1")
          (.getResponse)
          (pprint))
 
-    ;; retrieve a status map of all network interfaces
+    (println "retrieve asynchronously")
+    (snmp-get-next session
+      :async (proxy [ResponseListener] []
+               (onResponse [event]
+                 (println (str "async: " event))))
+      "1.3.6.1.2.1.31.1.1.1")
+
+
+    (println "retrieve a status map of all network interfaces")
     (->> (snmp-table-walk session
            "1.3.6.1.2.1.31.1.1.1.1"
            "1.3.6.1.2.1.31.1.1.1.2"
            "1.3.6.1.2.1.31.1.1.1.3")
          (pprint))
 
-    ;; table-walk with for-table-* utilities
+    (println "table-walk with for-table-* utilities")
     (let [table (snmp-table-walk session
                   "1.3.6.1.2.1.31.1.1.1.1"
                   "1.3.6.1.2.1.31.1.1.1.2"
@@ -32,5 +39,5 @@
                 (count r))))
       (println
        (for-table-with-column c table
-         (.getVariable c)))
-    )))
+         (.getVariable c))))
+    ))
