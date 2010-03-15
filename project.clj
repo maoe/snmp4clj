d9b2050be2f9c7d71b855f802994bf9357ce707a
maoe <maoe.maoe@gmail.com>
2010-03-15 16:29:53 +0900

    update to lein-swank 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [org.snmp4j/snmp4j "1.11"]
                  [org.clojars.maoe/funky "0.0.6"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [autodoc "0.7.0"]]
   :namespaces [snmp4j]
